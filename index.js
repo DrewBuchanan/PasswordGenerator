8478e3c057d60779a3d521da72cbf85e31d50e01
Drew <12664792+DrewBuchanan@users.noreply.github.com>
2024-11-18 02:01:46 -0600

    Add string shuffle function
    This is used after all characters are added to password to randomly distribute characters

--- index.js
+++ index.js
@@ -44,3 +44,15 @@ function copyToClipboard() {
     copyAlert.classList.add("animatemove");
     copyAlert.addEventListener("animationend", () => copyAlert.classList.remove("animatemove"), {once : true});
 }
+
+String.prototype.shuffle = function() {
+    var a = this.split(""), n = a.length;
+
+    for(var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
