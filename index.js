4a13a7cc39c9908e3411907f43c74b1157ca51ff
Drew <12664792+DrewBuchanan@users.noreply.github.com>
2024-11-18 01:07:00 -0600

    Create form content and styles for options

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordOne = document.querySelector("#password-one");
-let copyAlert = document.querySelector("#copy-alert");
-let copyButton = document.querySelector("#copy-button");
+const passwordOne = document.querySelector("#password-one");
+const copyAlert = document.querySelector("#copy-alert");
+const copyButton = document.querySelector("#copy-button");
+const lengthSlider = document.querySelector("#length");
+const lengthDisplay = document.querySelector("#length-value");
+const minimumSpecial = document.querySelector("#special-min");
+const minimumNumber = document.querySelector("#numbers-min");
+
+lengthSlider.addEventListener("input", (event) => {
+    lengthDisplay.value = event.target.value;
+    minimumNumber.max = Math.floor(event.target.value/4);
+    minimumSpecial.max = Math.floor(event.target.value/4);
+});
 
 function generatePasswords() {
     passwordOne.value = generatePassword();
